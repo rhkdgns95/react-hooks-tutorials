0359ed083490d04213ad0d8f869d43413323bda0
rhkdgns9489@gmail.com <rhkdgns9489@gmail.com>
2019-10-12 18:11:27 +0900

    #1.6 Run effects only once - MouseEvent.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import HookCounterTwo from "./Component/HookCounterTwo";
 import HookCounterThree from "./Component/HookCounterThree";
 import HookCounterFour from "./Component/HookCounterFour";
 import HookCounterOne from "./Component/HookCounterOne";
+import HookMouse from "./Component/HookMouse";
 
-const App =  () => <HookCounterOne />;
+const App =  () => <HookMouse />;
 
 export default App;
---
+++ src/Component/HookMouse/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+interface IPosition {
+    clientX: number;
+    clientY: number;
+}
+
+const useFetch = () => {
+    const [position, setPosition] = useState<IPosition>({clientX:0, clientY:0});
+    
+    const handleMouseMove = (event: MouseEvent) => {
+        const { target, clientX, clientY } = event;
+        setPosition({
+            clientX,
+            clientY
+        });
+    }
+    
+    useEffect(() => {
+        console.log("useEffect Called - Add MouseEvent");
+        window.addEventListener('mousemove', handleMouseMove);
+    }, [])
+
+    useEffect(() => {
+        console.log("useEffect Called - Update Position");
+    }, [position]);
+
+    return {
+        position,
+        handleMouseMove
+    };
+}
+const Styles:React.CSSProperties = {
+    width: '100vw',
+    height: '100vw',
+    backgroundColor: 'black',
+    color: 'white'
+}
+
+const HookMouse = () => {
+    const { position: { clientX, clientY }, handleMouseMove } = useFetch();
+    
+    return (
+        <div style={Styles}>
+            <h5>Mouse Position</h5>
+            <p>Hooks X - { clientX } Y- { clientY }</p>
+        </div>
+    );
+};
+
+export default HookMouse;
