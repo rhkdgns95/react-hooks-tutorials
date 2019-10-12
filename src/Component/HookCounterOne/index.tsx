0e12a88b6501b93abaaae67eae3bc5254da6dc66
rhkdgns9489@gmail.com <rhkdgns9489@gmail.com>
2019-10-12 17:36:48 +0900

    #1.5 Conditionally run effects.

--- src/Component/HookCounterOne/index.tsx
+++ src/Component/HookCounterOne/index.tsx
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from "react";
 
-const useFetch = (initCount: number) => {
+const useFetch = (initCount: number, initName: string) => {
     const [count, setCount] = useState<number>(initCount);
-    
+    const [name, setName] = useState<string>(initName);
+
     const onUpdateCount = (newCount: number) => {
         setCount(newCount);
     }
+
+    const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        const { target: { name, value }} = event;
+        setName(value);
+    }
     
     useEffect(() => {
+        console.log("useEffect()");
         document.title = `You clicked ${count} times`;
-    });
+    }, [count]);
 
     return {
         count,
-        onUpdateCount
+        onUpdateCount,
+        name,
+        onChange
     };
 }
 const HookCounterOne = () => {
-    const { count, onUpdateCount } = useFetch(0);
+    const { count, onUpdateCount, name, onChange } = useFetch(0, '');
 
     return (
         <div>
+            <input type={"text"} value={name} onChange={onChange}/>
             <button onClick={e => onUpdateCount(count + 1)}>Click {count} times</button>
         </div>
     );
